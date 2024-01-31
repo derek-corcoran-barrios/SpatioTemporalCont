8c12f4dee856bcf2d1dcfc552f578bb792bccd37
derek-corcoran-barrios <derek-corcoran-barrios@gmail.com>
2024-01-31 11:54:16 +0100

    Added the generate_dummy_stack function documentation

--- R/generate_dummy_stack.R
+++ R/generate_dummy_stack.R
@@ -1,3 +1,24 @@
+#' Generate Dummy Variable Stack from Categorical Raster
+#'
+#' This function takes a categorical raster and generates a stack of rasters with
+#' dummy variables, where each layer corresponds to one of the categorical variables.
+#'
+#' @param Rast A categorical raster (class: SpatRaster) representing landcover.
+#' @return A stack of rasters with dummy variables, where each layer corresponds to a
+#'         unique category in the input raster.
+#' @seealso \code{\link{SpatRaster}}, \code{\link{terra::ifel}}, \code{\link{purrr::reduce}}
+#'
+#' @examples
+#' # Load necessary libraries and create a sample categorical raster
+#' library(terra)
+#' sample_raster <- rast(matrix(sample(letters[1:5], 100, replace = TRUE), 10, 10))
+#' # Generate dummy variable stack
+#' dummy_stack <- generate_dummy_stack(sample_raster)
+#'
+#' @importFrom terra rast ifel
+#' @importFrom purrr reduce
+#'
+#' @export
 generate_dummy_stack <- function(Rast){
   if(!(class(Rast) %in% c("SpatRaster"))) {stop("the file has to be of SpatRaster class")}
 
@@ -11,7 +32,7 @@ generate_dummy_stack <- function(Rast){
   Stack <- list()
   for(i in 1:n_levels){
     Stack[[i]] <- Temp
-    Stack[[i]] <- ifel(Temp == Levels$cover[i], 1, 0)
+    Stack[[i]] <- terra::ifel(Temp == Levels$cover[i], 1, 0)
     names(Stack[[i]]) <- Levels$cover[i]
   }
   Stack <- purrr::reduce(Stack, c)
